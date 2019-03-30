81463b15c384aaba448e17e258900ecd094713bb
laurentheirendt <laurent.heirendt@uni.lu>
2019-03-30 08:56:58 +0100

    draft of yml script
    draft of runtests
    
    remove additional script commands

---
+++ .artenolis/runtests.sh
@@ -0,0 +1,11 @@
+# print out the version and the architecture
+echo "ARCH = $ARCH"
+echo "JULIA_VER = $JULIA_VER"
+
+# launch the test script
+if [ "$ARCH" == "Linux" ]; then
+    $ARTENOLIS_SOFT_PATH/julia/$JULIA_VER/bin/julia --color=yes -e 'using Pkg; Pkg.clone(pwd())'
+fi
+
+CODE=$?
+exit $CODE
