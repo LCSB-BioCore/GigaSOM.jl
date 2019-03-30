ed98e85a12a56b6c733642759195fe3aa0883748
laurentheirendt <laurent.heirendt@uni.lu>
2019-03-30 11:33:34 +0100

    debugging pwd

--- .artenolis/runtests.sh
+++ .artenolis/runtests.sh
@@ -4,6 +4,7 @@ echo "JULIA_VER = $JULIA_VER"
 
 # launch the test script
 if [ "$ARCH" == "Linux" ]; then
+    pwd
     $ARTENOLIS_SOFT_PATH/julia/$JULIA_VER/bin/julia --color=yes -e 'import Pkg; Pkg.add(pwd()); Pkg.rm("GigaSOM");'
 fi
 
