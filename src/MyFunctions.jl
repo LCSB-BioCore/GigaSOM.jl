a0772dcb0c64258c0100e1197d9596eae6fccff7
ohunewald <oliver.hunewald@lih.lu>
2019-04-09 14:53:29 +0200

    Simplified flowframe, added boxplot, cleaning column names, start testing SOM

--- src/MyFunctions.jl
+++ src/MyFunctions.jl
@@ -1,5 +1,7 @@
 
 "reads in FCS files from metada list and retuns a flowset. Add sampleid as column"
+# the package ReadFCS is very basic and only reads the marker names and not the description
+# column ! which is used in the R workflow
 # let's keep this function generic and only return the dict of dataframes (FCS files)
 function readflowset(flist)
     flowFrame = Dict()
@@ -18,9 +20,77 @@ function readflowset(flist)
             # using as Symbol
             # TODO: check if first char start with a number
             df[Symbol("_",k)] = v
+
         end
         flowfile["data"] = df
         flowFrame[fname] = flowfile
     end
     return flowFrame
+end
+
+"simpler version of readflowset, does the cleaning of the col names"
+function readflowset2(flist)
+    flowFrame = Dict()
+
+    # read all FCS files into flowFrame
+    for fname in flist # file list
+        flowrun = load(fname) # FCS file
+        flowDF = DataFrame(flowrun.data)
+
+        # replace chritical characters
+        # put "_" in front of colname in case it starts with a number
+        # TODO: check if string starts with a number 
+        names!(flowDF, [Symbol(replace(String(j), "-"=>"_")) for j in names(flowDF)])
+        names!(flowDF, [Symbol("_$i") for i in names(flowDF)])
+
+        flowFrame[fname] = flowDF
+
+    end
+    return flowFrame
+end
+
+
+"data transformation, currently only asinh"
+function transform_ff(flowset, method = "asinh", cofactor = 5)
+
+    # loop through every file in dict
+    # get the dataframe
+    # convert to matrix
+    # arcsinh transformation
+    # convert back to dataframe
+    for (k,v) in flowset
+        single_fcs = flowset[k]
+        colnames = names(single_fcs["data"]) # keep the column names
+        dMatrix = Matrix(single_fcs["data"])
+        # single_fcs["data"] = [(asinh(x)/cofactor) for x in dMatrix]
+        dMatrix = [(asinh(x)/cofactor) for x in dMatrix]
+
+        ddf = DataFrame(dMatrix)
+
+        names!(ddf, Symbol.(colnames))
+        single_fcs["data"] = ddf
+        flowset[k] = single_fcs
+    end
+end
+
+
+function transform_ff2(flowframe, method = "asinh", cofactor = 5)
+    # loop through every file in dict
+    # get the dataframe
+    # convert to matrix
+    # arcsinh transformation
+    # convert back to dataframe
+    for (k,v) in flowframe
+        fcs_df = flowframe[k]
+        colnames = names(fcs_df) # keep the column names
+        dMatrix = Matrix(fcs_df)
+        # single_fcs["data"] = [(asinh(x)/cofactor) for x in dMatrix]
+        dMatrix = [(asinh(x)/cofactor) for x in dMatrix]
+
+        ddf = DataFrame(dMatrix)
+
+        names!(ddf, Symbol.(colnames))
+        # single_fcs["data"] = ddf
+        flowframe[k] = ddf
+    end
 end
--- src/Test_FCS_load_and_transform.jl
+++ src/Test_FCS_load_and_transform.jl
@@ -9,18 +9,25 @@ FCS, we need to see what functionality is missing and
 extend this in the original package
 =#
 
-using FileIO
+
 Pkg.add("FCSFiles")
 Pkg.add("DataFrames")
 Pkg.add("CSV")
-Pkg.add("StatPlots")
+Pkg.add("StatsPlots")
+Pkg.add("Statistics")
+
+
 Pkg.add("MultivariateStats")
 
-cd("/home/ohunewald/work/GigaSOM_data/test_data")
+# cd("/home/ohunewald/work/GigaSOM_data/test_data")
+cd("/home/ohunewald/work/GigaSOM_data/PBMC8_fcs_files")
 
+using FileIO
+using FCSFiles
 using DataFrames
 using CSV
-using StatPlots
+using StatsPlots
+using Statistics
 using MultivariateStats
 include("MyFunctions.jl")
 
@@ -40,28 +47,6 @@ function exprs(args)
 end
 
 
-"data transformation, currently only asinh"
-function transform_ff(flowset, method = "asinh", cofactor = 5)
-
-    # loop through every file in dict
-    # get the dataframe
-    # convert to matrix
-    # arcsinh transformation
-    # convert back to dataframe
-    for (k,v) in flowset
-        single_fcs = flowset[k]
-        colnames = names(single_fcs["data"]) # keep the column names
-        dMatrix = Matrix(single_fcs["data"])
-        # single_fcs["data"] = [(asinh(x)/cofactor) for x in dMatrix]
-        dMatrix = [(asinh(x)/cofactor) for x in dMatrix]
-
-        ddf = DataFrame(dMatrix)
-
-        names!(ddf, Symbol.(colnames))
-        single_fcs["data"] = ddf
-        flowset[k] = single_fcs
-    end
-end
 
 " This function is a workaround for R's match"
 # this is a helper to find which sampleid has which condition
@@ -73,51 +58,61 @@ end
 # this is part of the original workflow in R
 ####################################################
 # materialize a csv file as a DataFrame
-md = CSV.File("metadata.xlsx") |> DataFrame
+# md = CSV.File("metadata.csv") |> DataFrame
+md = CSV.File("PBMC8_metadata.csv") |> DataFrame
 print(md)
 
 # load panel data
-panel = CSV.File("panel.csv") |> DataFrame
+# panel = CSV.File("panel.csv") |> DataFrame
+panel = CSV.File("PBMC8_panel.csv") |> DataFrame
 print(panel.Antigen)
 
 # extract lineage markers
 lineage_markers = panel.Antigen[panel.Lineage .== 1, : ]
+
+# for whatever reason lineage_markers are cast into 2d array,
+# flatten this array by using vec:
+lineage_markers = vec(lineage_markers)
+
 # remove critical characters
 lineage_markers = [replace(i, "-"=>"_") for i in lineage_markers]
 # place a '_' in front of each marker, as column names cannot start with a number
 lineage_markers = [("_$i") for i in lineage_markers]
 
-
-# TODO: remove critical characters in keys from flowrun
-# implement as core function in readflowset? or manually
-# as in original R workflow?
-#
-# for k in keys(flowrun.data)
-#     k = replace(k, "-"=>"_") for k in
-#
-# end
-
-
-fcs = readflowset(md.file_name)
-
+# fcs = readflowset(md.file_name)
+fcs = readflowset2(md.file_name)
 # compare state of transfromation
-file1data = fcs["file1.fcs"]["data"] # control before transfrom
-transform_ff(fcs, "ar")
-file2dat = fcs["file1.fcs"]["data"] # control after transform
+# file1data = fcs["file1.fcs"]["data"] # control before transfrom
+# transform_ff(fcs, "ar")
+transform_ff2(fcs, "ar")
+# file2dat = fcs["file1.fcs"]["data"] # control after transform
 
 # add sample_id as last column to DataFrame to keep track of samples
 # not by default to keep readflowset more generic
-for i in eachindex(md.Column1)
-    df = fcs[md.file_name[i]]["data"]
+for i in eachindex(md.file_name)
+    df = fcs[md.file_name[i]]
     df[:sample_id] = string(md.sample_id[i])
 end
+
 # check last columns
-file1data = fcs["file2.fcs"]["data"]
+# file1data = fcs["file2.fcs"]["data"]
+file1data = fcs["PBMC8_30min_patient1_BCR-XL.fcs"]
 print(file1data[95:100, 32:36])
 
 
-# TODO: barplot for samplesize
-# create DF with sample ids and cells counts and conditon
+########################################################
+# Barplot sample size
+########################################################
+df_barplot = DataFrame(filename = String[], size = Int[], condition = String[])
+
+for (k,v) in fcs
+    sid = md.sample_id[k .== md.file_name]
+    condition = md.condition[k .== md.file_name]
+    push!(df_barplot, (sid[1], size(v)[1], condition[1]) )
+end
+sort!(df_barplot)
+bar(df_barplot.filename, df_barplot.size, title="Numer of Cells", group=df_barplot.condition,xrotation=60)
+
 
 ####################################################################
 # PCA plot
@@ -125,17 +120,30 @@ print(file1data[95:100, 32:36])
 ####################################################################
 # put all df from dict into array
 dfall = []
-for (k,v) in fcs
-    print(k)
-    push!(dfall,v["data"])
+# some performance measures:
+# iterate over dict in for loop is much faster than using
+# list comprehension:
+#  0.000090 seconds (67 allocations: 2.266 KiB)
+#  0.079576 seconds (79.39 k allocations: 3.957 MiB)
+@time for (k,v) in fcs
+    push!(dfall,v)
 end
+
+# @time [push!(dfall,v) for (k,v) in fcs]
+
 dfall = vcat(dfall...)
+
+# reduce the dataset to lineage (and later) functional (state) markers
+cc = map(Symbol, lineage_markers)
+# cc = map(Symbol, ["_CD3(110:114)Dd", "_CD7(Yb176)Dd"])
+push!(cc, :sample_id)
+dfall = dfall[:, cc]
 
 dfall_median = aggregate(dfall, :sample_id, median)
 
 T = convert(Matrix, dfall_median)
 samples_ids = T[:,1]
-T_reshaped = permutedims(convert(Matrix{Float32}, T[:, 2:36]), [2, 1])
+T_reshaped = permutedims(convert(Matrix{Float32}, T[:, 2:10]), [2, 1])
 
 my_pca = fit(PCA, T_reshaped)
 
@@ -149,10 +157,10 @@ v1= df_pca.sample_id; v2=md.sample_id
 idxs = indexin(v1, v2)
 df_pca[:condition] = md.condition[idxs]
 
-@df df_pca scatter(:x1, :x2, group=:condition)
+StatsPlots.@df df_pca scatter(:x1, :x2, group=:condition)
 
 
-
+# classical_mds(T_reshaped,4)
 
 # names!(file1data, [Symbol("_$i") for i in 1:36])
 
@@ -187,3 +195,4 @@ df_pca[:condition] = md.condition[idxs]
 
 
 v1 = [8,6,7,11]; v2 = -10:10
+using JuliaInterpreter
