0197df94850b9adc4c98c50d9a657ec902929b37
laurentheirendt <laurent.heirendt@uni.lu>
2019-05-14 11:22:15 +0200

    add temporary doc for v0.0.4

---
+++ v0.0.4/search_index.js
@@ -0,0 +1,19 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "GigaSOM.jl",
+    "title": "GigaSOM.jl",
+    "category": "page",
+    "text": "(Image: GigaSOM.jl)"
+},
+
+{
+    "location": "#GigaSOM.jl-1",
+    "page": "GigaSOM.jl",
+    "title": "GigaSOM.jl",
+    "category": "section",
+    "text": "Huge-scale, high-performance flow cytometry clustering"
+},
+
+]}
