13170f554c0d4864e60b325a82d69addca48b442
laurentheirendt <laurent.heirendt@uni.lu>
2019-05-24 16:06:07 +0200

    add packages to gigasom

--- src/GigaSOM.jl
+++ src/GigaSOM.jl
@@ -1,10 +1,19 @@
-module GigaSOM
-export hello
+"""
+Main module for `GigaSOM.jl` - Huge-scale, high-performance flow cytometry clustering
+
+The documentation is here: http://LCSB-BioCore.github.io/GigaSOM.jl
 
 """
-    hello(who::String)
-Return "Hello, `who`".
-"""
-hello(who::String) = "Hello, $who"
+
+module GigaSOM
+
+    using Distributed
+    using DataFrames
+    using Distributions
+
+    include("errors.jl")
+    include("helpers.jl")
+    include("grids.jl")
+    include("kernels.jl")
 
 end # module
