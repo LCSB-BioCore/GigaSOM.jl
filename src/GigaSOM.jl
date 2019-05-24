e64113b5fcbf5421d47abb03ba679e664cfc1678
laurentheirendt <laurent.heirendt@uni.lu>
2019-05-24 16:16:41 +0200

    register files and modules in gigasom

--- src/GigaSOM.jl
+++ src/GigaSOM.jl
@@ -7,13 +7,24 @@ The documentation is here: http://LCSB-BioCore.github.io/GigaSOM.jl
 
 module GigaSOM
 
-    using Distributed
     using DataFrames
+    using Distances
+    using Distributed
+    using DistributedArrays
     using Distributions
+    using LinearAlgebra
+    using Plots
+    using ProgressMeter
+    using SOM
+    using StatsBase
+    using TensorToolbox
 
     include("errors.jl")
     include("helpers.jl")
     include("grids.jl")
     include("kernels.jl")
+    include("gigasoms.jl")
+    include("batch_som.jl")
+    include("parallel_som.jl")
 
 end # module
