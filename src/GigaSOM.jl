99efddc6f3a4098e13c827078b032d4650847a0b
laurentheirendt <laurent.heirendt@uni.lu>
2019-05-27 12:43:04 +0200

    add dependencies to gigasom module

--- src/GigaSOM.jl
+++ src/GigaSOM.jl
@@ -7,24 +7,40 @@ The documentation is here: http://LCSB-BioCore.github.io/GigaSOM.jl
 
 module GigaSOM
 
+    using CSV
     using DataFrames
     using Distances
     using Distributed
     using DistributedArrays
     using Distributions
+    using FileIO
+    using FCSFiles
+    using JuliaInterpreter
     using LinearAlgebra
-    using Plots
+    using MultivariateStats
     using ProgressMeter
     using SOM
     using StatsBase
+    using StatsPlots
+    using Statistics
     using TensorToolbox
 
-    include("errors.jl")
-    include("helpers.jl")
-    include("grids.jl")
-    include("kernels.jl")
-    include("gigasoms.jl")
     include("batch_som.jl")
     include("parallel_som.jl")
+
+    # include IO files
+    include("io/Fcs_helper.jl")
+
+    # include visualization files
+    include("visualization/Plotting.jl")
+
+    export # Fcs_helper
+        cleannames!,
+        readflowset,
+        create_daFrame
+
+    export # Plotting
+        plotcounts,
+        plotPCA
 
 end # module
