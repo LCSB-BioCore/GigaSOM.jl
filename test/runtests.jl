a16d61bf54977822cb0abe0d698eaec3207c038d
VascoVerissimo <vasco.verissimo@uni.lu>
2019-05-27 16:01:14 +0200

    WIP writting tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,13 @@
 using GigaSOM
 using Test
+
+cd("C:/Users/vasco.verissimo/ownCloud/PhD Vasco/CyTOF Project/CyTOF Data")
+df_codes = CSV.File("df_codes.csv") |> DataFrame
+
+
+cd("C:/Users/vasco.verissimo/work/git/hub/GigaSOM.jl/test/refdata")
+refdata_df_codes = CSV.File("refdata_df_codes.csv") |> DataFrame
+
+
+
+@test isapprox(refdata_df_codes, df_codes)
