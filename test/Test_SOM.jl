96a59c2ecfc96aadbe56971daefe3b054b1a60df
VascoVerissimo <vasco.verissimo@uni.lu>
2019-05-31 02:45:30 +0200

    partially transfered to runtests.jl

--- test/Test_SOM.jl
+++ test/Test_SOM.jl
@@ -1,29 +1,4 @@
-include("Fcs_load_and_transform.jl")
 
-(lineage_markers,)= getMarkers(panel)
-
-# only use lineage_markers for clustering
-cc = map(Symbol, lineage_markers)
-df_som = daf.fcstable[:,cc]
-
-som2 = initSOM(df_som, 10, 10, topol = :rectangular)
-
-# using classical som training without epochs
-@time som2 = trainSOM(som2, df_som, 100000)
-
-
-
-@time mywinners = mapToSOM(som2, df_som)
-CSV.write("cell_clustering_som.csv", mywinners)
-
-# myfreqs = SOM.classFrequencies(som2, daf.fcstable, :sample_id)
-
-codes = som2.codes
-df_codes = DataFrame(codes)
-names!(df_codes, Symbol.(som2.colNames))
-CSV.write("df_codes.csv", df_codes)
-CSV.write("mywinners.csv", mywinners)
-# CSV.write("myfreqs.csv", myfreqs)
 
 
 #=
