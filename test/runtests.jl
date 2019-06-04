7c86625386a5e65fdd9c0f905c650737c3a745e1
VascoVerissimo <vasco.verissimo@uni.lu>
2019-06-04 14:59:55 +0200

    seed fixed, test for df_codes and mywinners added for the original algorithm

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,15 @@
 using GigaSOM
 using Test
-
+using Random
 # cd("..")
+
+#fix the seed
+Random.seed!(1)
+
 include("Fcs_load_and_transform.jl")
 
-(lineage_markers,)= getMarkers(panel)
 
+(lineage_markers,)= getMarkers(panel)
 # only use lineage_markers for clustering
 cc = map(Symbol, lineage_markers)
 df_som = daf.fcstable[:,cc]
@@ -16,22 +20,24 @@ som2 = initSOM(df_som, 10, 10, topol = :rectangular)
 @time som2 = trainSOM(som2, df_som, 100000)
 
 @time mywinners = mapToSOM(som2, df_som)
-CSV.write("../gendata/cell_clustering_som.csv", mywinners)
+CSV.write("../test/gendata/cell_clustering_som.csv", mywinners)
 
 # myfreqs = SOM.classFrequencies(som2, daf.fcstable, :sample_id)
 
 codes = som2.codes
 df_codes = DataFrame(codes)
 names!(df_codes, Symbol.(som2.colNames))
-CSV.write("../gendata/df_codes.csv", df_codes)
-CSV.write("../gendata/mywinners.csv", mywinners)
+CSV.write("../test/gendata/df_codes.csv", df_codes)
+CSV.write("../test/gendata/mywinners.csv", mywinners)
 # CSV.write("myfreqs.csv", myfreqs)
 
 
-df_codes_test = CSV.File("../gendata/df_codes.csv") |> DataFrame
+df_codes_test = CSV.File("../test/gendata/df_codes.csv") |> DataFrame
+ref_df_codes = CSV.File("../test/refdata/ref_df_codes.csv") |> DataFrame
 
-refdata_df_codes = CSV.File("../gendata/refdata_df_codes.csv") |> DataFrame
+mywinners_test = CSV.File("../test/gendata/mywinners.csv") |> DataFrame
+ref_mywinners = CSV.File("../test/refdata/ref_mywinners.csv") |> DataFrame
 
+@test ref_df_codes == df_codes_test
 
-
-@test refdata_df_codes === df_codes
+@test ref_mywinners == mywinners_test
