8272141a173a32c322f22bd7714f6ea0739edf5a
VascoVerissimo <vasco.verissimo@uni.lu>
2019-06-05 16:50:43 +0200

    reduced epochs and added remove data files

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,26 +8,32 @@ Random.seed!(1)
 
 include("Fcs_load_and_transform.jl")
 
+# #test cleannames
+# for i in eachindex(lineage_markers)
+#         test_clean = @test !in("-",i)
+#         return test_clean
+# end
+
+
 
 (lineage_markers,)= getMarkers(panel)
 
 
-
 #BATCH & PARALLEL
 
-# p = addprocs(0)
+# p = addprocs(2)
 # only use lineage_markers for clustering
 cc = map(Symbol, lineage_markers)
 df_som = daf.fcstable[:,cc]
 df_som_large = vcat(df_som,df_som)
-df_som_large = vcat(df_som_large, df_som)
+# df_som_large = vcat(df_som_large, df_som)
 # topology is now always rectangular
 # som2 = initSOM_parallel(df_som, 10, 10)
 som2 = initSOM_parallel(df_som, 10, 10)
 
 # using batch som with epochs
-# @time som2 = trainSOM_parallel(som2, df_som, size(df_som)[1], epochs = 10)
-som2 = trainSOM_parallel(som2, df_som_large, size(df_som_large)[1], epochs = 10)
+@time som2 = trainSOM_parallel(som2, df_som, size(df_som)[1], epochs = 1)
+# som2 = trainSOM_parallel(som2, df_som_large, size(df_som_large)[1], epochs = 2)
 
 @time mywinners = mapToSOM(som2, df_som)
 CSV.write("../test/gendata/batch_cell_clustering_som.csv", mywinners)
@@ -50,3 +56,6 @@ batch_mywinners_test = CSV.File("../test/gendata/batch_mywinners.csv") |> DataFr
 
 # @test ref_batch_df_codes == batch_df_codes_test
 # @test ref_batch_mywinners == batch_mywinners_test
+
+rm("../test/data", recursive=true)
+rm("../test/gendata", recursive=true)
