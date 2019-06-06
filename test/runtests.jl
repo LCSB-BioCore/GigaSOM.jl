152a88f18a88134fc9b31c6b13a319f256de5d44
VascoVerissimo <vasco.verissimo@uni.lu>
2019-06-06 16:34:38 +0200

    reduced refData files

--- test/runtests.jl
+++ test/runtests.jl
@@ -40,13 +40,16 @@ names!(df_codes, Symbol.(som2.colNames))
 CSV.write(gendatapath*"/batch_df_codes.csv", df_codes)
 CSV.write(gendatapath*"/batch_mywinners.csv", mywinners)
 
-# CSV.write("test/refData/ref_batch_df_codes.csv", df_codes)
-# CSV.write("test/refData/ref_batch_mywinners.csv", mywinners)
+#Create the refData files when needed
+# CSV.write("test/refData/ref_batch_df_codes.csv", first(df_codes, 10))
+# CSV.write("test/refData/ref_batch_mywinners.csv", first(mywinners, 10))
 
 ref_batch_df_codes = CSV.File("test/refData/ref_batch_df_codes.csv") |> DataFrame
 ref_batch_mywinners = CSV.File("test/refData/ref_batch_mywinners.csv") |> DataFrame
 batch_df_codes_test = CSV.File(gendatapath*"/batch_df_codes.csv") |> DataFrame
+batch_df_codes_test = first(batch_df_codes_test, 10)
 batch_mywinners_test = CSV.File(gendatapath*"/batch_mywinners.csv") |> DataFrame
+batch_mywinners_test = first(batch_mywinners_test, 10)
 
 @test ref_batch_df_codes == batch_df_codes_test
 @test ref_batch_mywinners == batch_mywinners_test
