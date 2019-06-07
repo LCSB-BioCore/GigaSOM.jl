5c17acc744617e0a4c45f11ee6d531729c544c1b
ohunewald <oliver.hunewald@lih.lu>
2019-06-07 17:35:13 +0200

    modifications for PR

--- test/Test_SOM_parallel.jl
+++ test/Test_SOM_parallel.jl
@@ -1,23 +1,15 @@
+using Test
 using Distributed
 
 p = addprocs(2)
 
-# @everywhere using Pkg
-# @everywhere Pkg.add("DistributedArrays")
-# @everywhere Pkg.add("DataFrames")
-# @everywhere Pkg.add("Distances")
-
 @everywhere using DistributedArrays
 @everywhere using GigaSOM
-# @everywhere using DataFrames
-# @everywhere using Distances
-# @everywhere using Revise
 
 # only use lineage_markers for clustering
 cc = map(Symbol, lineage_markers)
 df_som = daf.fcstable[:,cc]
-df_som = vcat(df_som, df_som)
-df_som = vcat(df_som, df_som)
+# df_som = vcat(df_som, df_som)
 
 som2 = initSOM_parallel(df_som, 10, 10)
 
@@ -29,6 +21,8 @@ CSV.write("cell_clustering_som.csv", mywinners)
 # myfreqs = SOM.classFrequencies(som2, daf.fcstable, :sample_id)
 
 codes = som2.codes
+@test size(codes) == (100,10)
+
 df_codes = DataFrame(codes)
 names!(df_codes, Symbol.(som2.colNames))
 CSV.write("df_codes.csv", df_codes)
