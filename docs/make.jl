df3fca1721813e2ded89dee96c9a6294a4b3d0e8
VascoVerissimo <vasco.verissimo@uni.lu>
2019-06-17 18:32:38 +0200

    updated make.jl file for structure

--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,28 @@
-using Documenter
+using Documenter, GigaSOM
 
-makedocs(modules = [Documenter],
-         sitename="GigaSOM.jl")
+makedocs(modules = [GigaSOM],
+         clean = false,
+         format = :html,
+         assets = ["assets/icongigasom.ico"],
+         sitename = "GigaSOM.jl",
+         authors = "The developers of GigaSOM.jl",
+         pages = [
+                "Introduction" => "index.md",
+                "Tutorial" => "tutorials/tutorial.md",
+                "API" => Any[
+                        "IO" => "api/io.md",
+                        "Types" => "api/types.md",
+                        "Batch/Parallel SOM" => "api/soms.md",
+                        "Visualisation" => "api/visualisation.md"
+                        ],
+                "License" => "LICENSE.md"
+                ],
+        html_cannonical = "https://lcsb-biocore.github.io/GigaSOM.jl/stable/",
+        )
 
 deploydocs(
     repo = "github.com/LCSB-BioCore/GigaSOM.jl.git",
+    julia = "1.1.0",
     target = "build",
     branch = "gh-pages",
     devbranch = "origin/develop",
