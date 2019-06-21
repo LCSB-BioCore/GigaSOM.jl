285d1ecbc1e8b720dacebaeaa811f055a8c898e5
VascoVerissimo <vasco.verissimo@uni.lu>
2019-06-21 14:39:24 +0200

    updated with new files names

--- src/GigaSOM.jl
+++ src/GigaSOM.jl
@@ -25,28 +25,34 @@ module GigaSOM
     # using ProgressMeter
     # using TensorToolbox
 
-    include("types.jl")
-    include("parallel_som.jl")
+    include("structs.jl")
+    include("core.jl")
+    include("satellites.jl")
 
     # include IO files
-    include("io/Fcs_helper.jl")
+    include("io/input.jl")
+    include("io/process.jl")
 
     # include visualization files
-    # include("visualization/Plotting.jl")
+    # include("visualization/plotting.jl")
 
-    export # ext/som
-        mapToGigaSOM,
+    export #core
         initGigaSOM,
-        trainGigaSOM
+        trainGigaSOM,
+        mapToGigaSOM
 
-    export # Fcs_helper
-        cleanNames!,
-        readFlowset,
-        createDaFrame,
-        getMarkers,
+    export # structs
         daFrame
 
-    export # Plotting
+    export #input
+        readFlowset
+
+    export #satellites
+    cleanNames!,
+    createDaFrame,
+    getMarkers
+
+    export # plotting
         plotCounts,
         plotPCA
 
