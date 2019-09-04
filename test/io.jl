1ebbc676349d0b2a0429b5267d45be640196c9f5
laurentheirendt <laurent.heirendt@uni.lu>
2019-09-04 10:39:34 +0200

    bring back essential sections of test

--- test/io.jl
+++ test/io.jl
@@ -3,15 +3,53 @@
 # Load and transform
 # build the general workflow to have the data ready
 
-# ENV["PYTHON"] = Sys.which("PYTHON")
-ENV["PYTHON"] = "/usr/local/bin/python3.6"
+#create genData and data folder and change dir to dataPath
+cwd = pwd()
+if occursin("jenkins", homedir()) || "TRAVIS" in keys(ENV)
+    genDataPath = mktempdir()
+    dataPath = mktempdir()
+else
+    if !occursin("test", cwd)
+        cd("test")
+        cwd = pwd()
+    end
+    dataFolders = ["genData", "data"]
+    for dir in dataFolders
+        if !isdir(dir)
+            mkdir(dir)
+        end
+    end
+    genDataPath = cwd*"/genData"
+    dataPath = cwd*"/data"
+end
 
-using GigaSOM, DataFrames, XLSX, CSV, Test, Random, Distributed, SHA, JSON, PyCall
+# fetch the required data for testing and download the zip archive and unzip it
+dataFiles = ["PBMC8_metadata.xlsx", "PBMC8_panel.xlsx", "PBMC8_fcs_files.zip"]
+for f in dataFiles
+    if !isfile(f)
+        download("http://imlspenticton.uzh.ch/robinson_lab/cytofWorkflow/"*f, f)
+        if occursin(".zip", f)
+            run(`unzip PBMC8_fcs_files.zip`)
+        end
+    else
+    end
+end
 
-fcsparser = pyimport("fcsparser")
+@testset "Checksums" begin
+    cd(dataPath)
+    filesNames = readdir()
+    csDict = Dict()
+    for f in filesNames
+        cs = bytes2hex(sha256(f))
+        csDict[f] = cs
+    end
+    cd(cwd*"/checkSums")
+    csTest = JSON.parsefile("csTest.json")
+    @test csDict == csTest
+    cd(cwd)
+end
 
-dataPath = ("../data_felD1")
-# dataPath = ("../PBMC8_fcs_files")
+dataPath = ("../PBMC8_fcs_files")
 cd(dataPath)
 md = DataFrame(XLSX.readtable("metadata.xlsx", "Sheet1", infer_eltypes=true)...)
 panel = DataFrame(XLSX.readtable("panel.xlsx", "Sheet1", infer_eltypes=true)...)
