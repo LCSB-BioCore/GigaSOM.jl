0fd580e73ef79d930e652c4bb02d488be9cd23d5
laurentheirendt <laurent.heirendt@uni.lu>
2019-09-04 10:26:22 +0200

    move getmetadata to process.jl

--- src/io/process.jl
+++ src/io/process.jl
@@ -125,3 +125,62 @@ function getMarkers(panel)
     return lineageMarkers, functionalMarkers
 
 end
+
+"""
+    getMetaData(f)
+Collect the meta data information in a more user friendly format.
+
+# Arguments:
+- `f`: input structure with `.params` and `.data` fields
+"""
+function getMetaData(f)
+
+    # declarations and initializations
+    meta = f.params
+    metaKeys = keys(meta)
+    channel_properties = []
+    defaultValue = "None"
+
+    # determine the number of channels
+    pars = parse(Int, strip(join(meta["\$PAR"])))
+
+    # determine the range of channel numbers
+    channel_numbers = 1:pars
+
+    # determine the channel properties
+    for (key,) in meta
+        if key[1:3] == "\$P1"
+            if !occursin(key[4], "0123456789")
+                push!(channel_properties, key[4:end])
+            end
+        end
+    end
+
+    # define the column names
+    column_names = ["\$Pn$p" for p in channel_properties]
+
+    # create a data frame
+    df = DataFrame([Vector{Any}(undef, 0) for i = 1:length(column_names)])
+
+    # fill the data frame
+    for ch in channel_numbers
+        # build first each row of the datatable
+        tmpV = []
+        for p in channel_properties
+            if "\$P$ch$p" in metaKeys
+                tmp = meta["\$P$ch$p"]
+            else
+                tmp = defaultValue
+            end
+            push!(tmpV, tmp)
+        end
+
+        # push the row to the dataframe
+        push!(df, tmpV)
+    end
+
+    # set the names of the df
+    names!(df, Symbol.(column_names))
+
+    return df
+end
