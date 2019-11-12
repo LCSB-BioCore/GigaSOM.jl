cd3189abb743211be89879bfaeb3fc67ea7cfd8e
ohunewald <oliver.hunewald@lih.lu>
2019-11-12 13:00:55 +0100

    added dist matrix test

--- test/functionTest.jl
+++ test/functionTest.jl
@@ -13,4 +13,10 @@
 
         @test isapprox(GigaSOM.gaussianKernel(1, 6.0), 0.17603266; atol = 0.001)
     end
+
+    @testset "dist matrix" begin
+        g = GigaSOM.gridRectangular(2,2)
+        dm = GigaSOM.distMatrix(g, false)
+        @test size(dm) == (4,4)
+    end
 end
