0271b8703a6096da74fcf18e1b6625ca1147d5c9
ohunewald <oliver.hunewald@lih.lu>
2019-11-12 13:19:02 +0100

    test convertTrainingData

--- test/functionTest.jl
+++ test/functionTest.jl
@@ -14,9 +14,15 @@
         @test isapprox(GigaSOM.gaussianKernel(1, 6.0), 0.17603266; atol = 0.001)
     end
 
-    @testset "dist matrix" begin
+    @testset "distMatrix" begin
         g = GigaSOM.gridRectangular(2,2)
         dm = GigaSOM.distMatrix(g, false)
         @test size(dm) == (4,4)
+    end
+
+    @testset "convertTrainingData" begin
+        df = DataFrame(Col1 = rand(5), Col2 = rand(5), Col3 = rand(5))
+        df = GigaSOM.convertTrainingData(df)
+        @test typeof(df) == Array{Float64,2}
     end
 end
