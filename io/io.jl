032d38b540c38f2c5a56eef1efd9889723442df4
laurentheirendt <laurent.heirendt@uni.lu>
2019-11-19 14:58:17 +0100

    load draft of parallel io

---
+++ io/io.jl
@@ -0,0 +1,37 @@
+using Distributed
+#XLSX
+using FCSFiles
+
+# local read data function
+nWorkers = 2
+
+addprocs(nWorkers)
+
+@everywhere function loadData(fileName)
+    @info fileName
+    return ones(2,2)
+end
+
+# read directory
+listFiles = readdir("fcs")
+
+#md = DataFrame(XLSX.readtable("PBMC8_metadata.xlsx", "Sheet1", infer_eltypes=true)...)
+#panel = DataFrame(XLSX.readtable("PBMC8_panel.xlsx", "Sheet1", infer_eltypes=true)...)
+
+#lineageMarkers, functionalMarkers = getMarkers(panel)
+
+#fcsRaw = readFlowset(md.file_name)
+#cleanNames!(fcsRaw)
+
+# create daFrame file
+#daf = createDaFrame(fcsRaw, md, panel)
+
+R = Vector{Any}(undef,nworkers())
+
+# load files in parallel
+N = convert(Int64, length(listFiles)/nWorkers)
+@sync begin
+        for (idx, pid) in enumerate(workers())
+            R[idx] =  fetch(@spawnat pid loadData(listFiles[(idx-1)*N+1: idx*N]))
+        end
+end
