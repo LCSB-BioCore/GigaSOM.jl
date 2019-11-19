2837eb9c85a8b476e270b919dd215461c0c80c41
laurentheirendt <laurent.heirendt@uni.lu>
2019-11-19 16:23:37 +0100

    include other functions for loading fcs files in parallel

--- io/io.jl
+++ io/io.jl
@@ -1,41 +1,46 @@
-using Distributed, XLSX, DataFrames
+using Distributed, XLSX, DataFrames, FileIO
 
 # read directory
 #listFiles = readdir("fcs")
 
 md = DataFrame(XLSX.readtable("PBMC8_metadata.xlsx", "Sheet1", infer_eltypes=true)...)
-listFiles = md.file_name
-#panel = DataFrame(XLSX.readtable("PBMC8_panel.xlsx", "Sheet1", infer_eltypes=true)...)
+panel = DataFrame(XLSX.readtable("PBMC8_panel.xlsx", "Sheet1", infer_eltypes=true)...)
+
+cd("fcs")
 
 # local read data function
 nWorkers = 2
 
 addprocs(nWorkers)
-@everywhere using  DataFrames, FCSFiles
+@everywhere using GigaSOM,  DataFrames, FCSFiles
 
-@everywhere function loadData(fileName, sid)
-    @info string(sid) * " - " * string(fileName)
+@everywhere function loadData(md, panel)
 
-    return ones(2,2)
+    # load FCS file
+    fcsRaw = readFlowset(md.file_name)
+
+    # clean names
+    cleanNames!(fcsRaw)
+
+    # create daFrame
+    daf = createDaFrame(fcsRaw, md, panel)
+
+    # return a random sample
+    gridSize = 100
+    nSamples = convert(Int64, floor(gridSize/nworkers()))
+    return daf.fcstable[rand(1:nSamples, nSamples), :]
 end
 
-
 #lineageMarkers, functionalMarkers = getMarkers(panel)
-
-#fcsRaw = readFlowset(md.file_name)
-#cleanNames!(fcsRaw)
-
-# create daFrame file
-#daf = createDaFrame(fcsRaw, md, panel)
-
 R = Vector{Any}(undef,nworkers())
 
 # load files in parallel
-N = convert(Int64, length(listFiles)/nWorkers)
+N = convert(Int64, length(md.file_name)/nWorkers)
 @sync begin
         for (idx, pid) in enumerate(workers())
-            R[idx] =  fetch(@spawnat pid loadData(listFiles[(idx-1)*N+1:idx*N], md.sample_id[(idx-1)*N+1:idx*N]))
+            R[idx] =  fetch(@spawnat pid loadData(md[(idx-1)*N+1:idx*N, :], panel))
         end
 end
 
+cd("..")
 rmprocs(workers())
