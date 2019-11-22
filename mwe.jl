dca6fe6982b94bd8b57ef45d60d7e73ab2b247e3
laurentheirendt <laurent.heirendt@uni.lu>
2019-11-22 13:57:34 +0100

    support for fcs files

--- mwe.jl
+++ mwe.jl
@@ -2,37 +2,40 @@ try
     using Distributed
     using FileIO
     using CSVFiles
+    using FCSFiles
 catch
     import Pkg;
     Pkg.add("Distributed")
     Pkg.add("FileIO")
     Pkg.add("CSVFiles")
+    Pkg.add("FCSFiles")
 end
 
 
 using Distributed, FileIO
 
 # prepare the workers
-nWorkers = 2
+nWorkers = 4
+type = "fcs"
 addprocs(nWorkers)
 @everywhere using FileIO
 
 # define a custom load function
-@everywhere function loadData(id, fileNames)
+@everywhere function loadData(id, fileNames, type)
     for fn in fileNames
-        in = load(fn)
+        in = load(type*"/"*fn)
     end
     return ones(id, id)
 end
 
 R = Vector{Any}(undef,nworkers())
-content = readdir("csv")
+content = readdir(type)
 
 # load files in parallel
 N = convert(Int64, length(content)/nWorkers)
 @time begin
     @sync for (idx, pid) in enumerate(workers())
-        @async R[idx] = fetch(@spawnat pid loadData(idx, content[(idx-1)*N+1:idx*N]))
+        @async R[idx] = fetch(@spawnat pid loadData(idx, content[(idx-1)*N+1:idx*N], type))
     end
 end
 
