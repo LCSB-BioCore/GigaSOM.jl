654796274f2d35ba88cc621f820feb027fe2046a
laurentheirendt <laurent.heirendt@uni.lu>
2019-11-26 22:50:42 +0100

    new trick working version

--- mwe.jl
+++ mwe.jl
@@ -15,21 +15,18 @@ end
 using Distributed, FileIO
 
 # prepare the workers
-nWorkers = 2
-type = "csv"
+nWorkers = 4
+type = "fcs"
 addprocs(nWorkers, topology=:master_worker)
-@everywhere import FileIO
+@everywhere using FileIO, FCSFiles
+#@everywhere import Pkg
+#@everywhere Pkg.activate("FileIO")
 
 @info "Number of workers: $nWorkers"
 
 # define a custom load function
-@everywhere function loadData(id, fileNames, type, precompileFlag)
-    if precompileFlag
-        for fn in fileNames
-        @show fn
-        @time in = FileIO.load(type*"/"*fn)
-        end
-    end
+@everywhere function loadData(id, fn, type)
+    @time in = FileIO.load(type*"/"*fn[1])
     return ones(id, id)
 end
 
@@ -42,12 +39,24 @@ N = convert(Int64, length(content)/nWorkers)
 @info "Benchmarking ..."
 
 @time begin
-# load files in parallel
-    @sync for (idx, pid) in enumerate(workers())
-        @async R[idx] = fetch(@spawnat pid loadData(idx, content[(idx-1)*N+1:idx*N], type, true))
+    #=
+    @time @sync begin
+        @async R[1] = fetch(@spawnat 2 loadData(2, content[1:1], type, true))
+        @async R[2] = fetch(@spawnat 3 loadData(3, content[2:2], type, true))
     end
+    @info "preloading done"
+    =#
+    # load files in parallel
+@sync for (idx, pid) in enumerate(workers())
+   #@async begin
+       @async for k in (idx-1)*N+1:idx*N
+            #remotecall_fetch(loadData, pid, pid, content[k:k], type)
+            R[idx] = fetch(@spawnat pid loadData(idx, content[k:k], type) )
+        end
+    #end
 end
 
+end
 # fetch
 @info R
 
