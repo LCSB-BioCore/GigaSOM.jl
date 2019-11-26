705c8c74755db0db989e0f22fa292b2f1e808536
laurentheirendt <laurent.heirendt@uni.lu>
2019-11-26 22:58:17 +0100

    change of input arg

--- mwe.jl
+++ mwe.jl
@@ -17,14 +17,14 @@ using Distributed, FileIO
 # prepare the workers
 nWorkers = 2
 type = "fcs"
-addprocs(nWorkers, topology=:master_worker)
+addprocs(nWorkers) #, topology=:master_worker)
 @everywhere using FileIO #, FCSFiles
 
 @info "Number of workers: $nWorkers"
 
 # define a custom load function
 @everywhere function loadData(id, fn, type)
-    @time in = FileIO.load(type*"/"*fn[1])
+    @time in = FileIO.load(type*"/"*fn)
     return ones(id, id)
 end
 
@@ -38,7 +38,7 @@ N = convert(Int64, length(content)/nWorkers)
 
 @time @sync for (idx, pid) in enumerate(workers())
     @async for k in (idx-1)*N+1:idx*N
-        R[idx] = fetch(@spawnat pid loadData(idx, content[k:k], type) )
+        R[idx] = fetch(@spawnat pid loadData(idx, content[k], type) )
     end
 end
 
