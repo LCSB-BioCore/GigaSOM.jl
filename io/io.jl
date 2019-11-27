3ab59e0ec458a2cefe4b7bc87e0bbda973067648
ohunewald <oliver.hunewald@lih.lu>
2019-11-27 08:59:17 +0100

    added randMatrix, initGigaSOM and trainGigaSOM

--- io/io.jl
+++ io/io.jl
@@ -28,7 +28,16 @@ N = convert(Int64, length(md.file_name)/nWorkers)
     @async R[idx] = fetch(@spawnat pid begin loadData(md.file_name[(idx-1)*N+1:idx*N],md, panel) end)
 end
 
+# Simulate the concatenation of return samples for testing only
+allRand = rand(10,10)
 
+som = initGigaSOM(allRand, 10, 10)
+
+# som2 = trainGigaSOM()
+Rsom = Vector{Any}(undef,nworkers())
+@time @sync for (idx, pid) in enumerate(workers())
+    @async Rsom[idx] = fetch(@spawnat pid begin trainGigaSOM(som, R[idx][2]) end) 
+end
 
 rmprocs(workers())
 
