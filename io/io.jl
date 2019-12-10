0c70eded0c183efdc62c9730beeca67d435d83a0
ohunewald <oliver.hunewald@lih.lu>
2019-12-10 15:30:01 +0100

    implemented random sampling from ref

--- io/io.jl
+++ io/io.jl
@@ -26,12 +26,22 @@ N = convert(Int64, (length(md.file_name)/nWorkers))
     @async R[idx] = fetch(@spawnat pid begin loadData(md.file_name[(idx-1)*N+1:idx*N],md, panel) end)
 end
 
-# Simulate the concatenation of return samples for testing only
-# init codes: size is 10x10 x number of params
-# each code contains vector of params -> 100 x length(lineageMarkers)
-allRand = rand(100, length(lineageMarkers))
+# Get n random samples from m workers in R:
+samplesPerWorker = Int(length(R))
+# sampleList defines wich data in R ref has to return one random sample
+# TODO: put this into core.jl
+sampleList = rand(1:samplesPerWorker, 100)
+X = zeros(100, length(lineageMarkers))
 
-som = initGigaSOM(allRand, 10, 10)
+for i in 1:length(sampleList)
+    element = sampleList[i]
+    # dereference and get one random sample from matrix
+    Y = R[element].x[rand(1:size(R[element].x, 1), 1),:]
+    # convert Y into vector
+    X[i, :] = vec(Y)
+end
+
+som = initGigaSOM(X, 10, 10)
 
 #------ trainGigaSOM() -------------------------
 # define the columns to be used for som training
