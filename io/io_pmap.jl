37fff818830617c17959442ecfed0d44476d8b93
ohunewald <oliver.hunewald@lih.lu>
2019-12-11 08:07:31 +0100

    added io pmap file

---
+++ io/io_pmap.jl
@@ -0,0 +1,63 @@
+
+using GigaSOM, DataFrames, XLSX, CSV, Test, Random, Distributed, SHA, JSON
+
+checkDir()
+cwd = pwd()
+
+# dataPath = "/Users/ohunewald/work/data_felD1/"
+dataPath = "/Users/ohunewald/work/artificial_data_cytof/"
+cd(dataPath)
+md = DataFrame(XLSX.readtable("metadata.xlsx", "Sheet1", infer_eltypes=true)...)
+panel = DataFrame(XLSX.readtable("panel.xlsx", "Sheet1", infer_eltypes=true)...)
+
+lineageMarkers, functionalMarkers = getMarkers(panel)
+
+nWorkers = 2
+addprocs(nWorkers, topology=:master_worker)
+@everywhere using GigaSOM, FCSFiles
+
+
+@everywhere begin
+    function rand_det(n)
+        println(n)
+        return 1
+    end
+end
+
+
+determinants = pmap(rand_det, 1:10)
+
+@info "processes added"
+
+R = Vector{Any}(undef,nworkers())
+
+N = convert(Int64, (length(md.file_name)/nWorkers))
+
+@time @sync for (idx, pid) in enumerate(workers())
+    @async R[idx] = fetch(@spawnat pid begin loadData(md.file_name[(idx-1)*N+1:idx*N],md, panel) end)
+end
+
+# Get n random samples from m workers in R:
+samplesPerWorker = Int(length(R))
+# sampleList defines wich data in R ref has to return one random sample
+# TODO: put this into core.jl
+sampleList = rand(1:samplesPerWorker, 100)
+X = zeros(100, length(lineageMarkers))
+
+for i in 1:length(sampleList)
+    element = sampleList[i]
+    # dereference and get one random sample from matrix
+    Y = R[element].x[rand(1:size(R[element].x, 1), 1),:]
+    # convert Y into vector
+    X[i, :] = vec(Y)
+end
+
+som = initGigaSOM(X, 10, 10)
+
+#------ trainGigaSOM() -------------------------
+# define the columns to be used for som training
+cc = map(Symbol, lineageMarkers)
+# R holds the reference to the dataset for each worker
+@time som = trainGigaSOM(som, R, cc) 
+
+rmprocs(workers())
