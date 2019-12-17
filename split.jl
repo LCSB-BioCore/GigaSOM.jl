94060e7143a897959a778e911987236ce3bf0b11
laurentheirendt <laurent.heirendt@uni.lu>
2019-12-17 16:59:19 +0100

    programming of map of splitting the files

--- split.jl
+++ split.jl
@@ -1,51 +1,89 @@
 location = ENV["HOME"]*"/artificial_data_cytof"
-fileType = ".fcs"
 binFileType = ".jls"
-
-nWorkers = 4
+nWorkers = 3
 
 fileDir = readdir(location)
 
 mdFileName = location*"/metadata.xlsx"
 panelFileName = location*"/panel.xlsx"
 
-using GigaSOM, FileIO, Test
+using GigaSOM, FileIO, Test, Serialization
 md = GigaSOM.DataFrame(GigaSOM.XLSX.readtable(mdFileName, "Sheet1")...)
 panel = GigaSOM.DataFrame(GigaSOM.XLSX.readtable(panelFileName, "Sheet1")...)
 
-# count the number of FCS files
-nFiles = 0
-for file in fileDir
-    global nFiles
-    if file[end-3:end] == fileType
-        nFiles = nFiles + 1
-    end
+# read in the entire dataset
+in = readFlowset(md.file_name)
+fileNames = collect(keys(in))
+
+@info " > Input: $(length(fileNames)) files"
+
+# get the total size of the data set
+totalSize = 0
+inSize = []
+for name in fileNames
+    global totalSize
+    totalSize += size(in[name])[1]
+    push!(inSize, size(in[name])[1])
 end
 
-# read the file
-fileName = "file1"
-df = FileIO.load(fileName*fileType)
-
-# write the dataframe as a binary file
-open(f -> serialize(f,df), fileName*binFileType, "w")
-
-y = open(deserialize, fileName*binFileType)
-
-@test y.data == df.data
-@test y.params == df.params
-
-# determine length of the data
-keyArray = collect(keys(df.data))
-L = length(df.data[keyArray[1]])
-
 # determine the size per file
-fileL = Int(floor(L/nWorkers))
+fileL = Int(floor(totalSize/nWorkers))
 
 # determine the size of the last (residual) file
-lastFileL = Int(fileL + L - nWorkers * fileL)
+lastFileL = Int(fileL + totalSize - nWorkers * fileL)
+
+@info " > # of workers: $nWorkers"
+@info " > Regular row count: $fileL cells"
+@info " > Last file row count: $lastFileL cells"
+
+# determine the running sum of the file sizes
+runSum = []
+tmpSum = 0
+for indivSize in inSize
+    global tmpSum
+    tmpSum += indivSize
+    push!(runSum, tmpSum)
+end
+
+# establish an index map
+limitFileIndex = 0
+fileEnd = 1
+for worker in 1:nWorkers
+    global limitFileIndex, md, fileEnd
+    iStart = Int((worker - 1) * fileL + 1)
+    iEnd = Int(worker * fileL)
+
+    if worker == nWorkers
+        iEnd = iStart + lastFileL - 1
+    end
+    @info " -----------------------------"
+    @info " >> Generating input-$worker.jls"
+    @info " -----------------------------"
+    @info " > iStart: $iStart; iEnd: $iEnd"
+
+    fileStart = fileEnd
+    for (k, tmpSum) in enumerate(runSum)
+        if tmpSum >= iEnd
+            @info " + Break point file: $(md.file_name[k])"
+            fileEnd = k
+            break;
+        end
+    end
+    if worker == nWorkers
+        fileEnd = length(md.file_name)
+    end
+
+    @info " > fileStart: $fileStart; fileEnd: $fileEnd"
+end
 
 # split the file properly speaking
+#=
 out = Dict()
 for key in keyArray
     out[key] = df.data[key][1:fileL]
 end
+
+open(f -> serialize(f,out), "out.jls", "w")
+y = open(deserialize, "out.jls")
+@test y == outa
+=#
