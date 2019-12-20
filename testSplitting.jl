402f2ba4ce3a309bc117e8caa93b4128953d404b
laurentheirendt <laurent.heirendt@uni.lu>
2019-12-20 14:25:26 +0100

    adding removal of generated files

--- testSplitting.jl
+++ testSplitting.jl
@@ -19,13 +19,27 @@ totalSize, inSize, runSum = getTotalSize(md, 0)
 @test inSize == [500, 800, 150, 200, 625, 330, 290, 400]
 @test runSum == [500, 1300, 1450, 1650, 2275, 2605, 2895, 3295]
 
-localStartVect, localEndVect = generateIO(fileNames, nWorkers, true, 0, true)
+localStartVect, localEndVect = generateIO(fileNames, nWorkers, true, 1, true)
 
 # test if the differences between the local indices correspond
 @test sum(localEndVect - localStartVect) + length(localEndVect) == totalSize
 
 @test localStartVect == [1, 275, 1, 50, 324, 598, 1, 72, 1, 196, 1, 270, 544, 1, 193, 1, 137, 1, 121]
 @test localEndVect == [274, 500, 49, 323, 597, 800, 71, 150, 195, 200, 269, 543, 625, 192, 330, 136, 290, 120, 400]
+
+# test if the data corresponds
+readFlowset(md.file_name)
+
+# remove all the files
+for k in 1:nWorkers
+    try
+        printstyled("> Removing input-$k.jls ... ", color=:yellow)
+        rm("input-$k.jls")
+        printstyled("Done.\n", color=:green)
+    catch
+        printstyled("(file does not exist - skipping).\n", color=:red)
+    end
+end
 
 # simple concatenation
 nWorkers = 1
