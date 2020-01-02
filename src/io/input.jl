8c3a5714dc10cfd64344574be2f5d364edb701b7
ohunewald <oliver.hunewald@lih.lu>
2020-01-02 09:50:20 +0100

    added function readFlowFrame(filename) for single fcs file

--- src/io/input.jl
+++ src/io/input.jl
@@ -32,3 +32,37 @@ function readFlowset(filenames)
 
     return flowFrame
 end
+
+
+"""
+    readFlowFrame(filename)
+
+Create a dictionary with a single flowframe
+
+# Arguments:
+- `filename`: string
+"""
+function readFlowFrame(filename)
+
+    flowFrame = Dict()
+
+    # read single FCS file into flowFrame
+    flowrun = FileIO.load(filename) # FCS file
+
+    # get metadata
+    # FCSFiles returns a dict with coumn names as key
+    # As the dict is not in order, use the name column form meta
+    # to sort the Dataframe after cast.
+    meta = getMetaData(flowrun)
+    markers = meta[:,1]
+    markersIsotope = meta[:,5]
+    flowDF = DataFrame(flowrun.data)
+    # sort the DF according to the marker list
+    flowDF = flowDF[:, Symbol.(markersIsotope)]
+    cleanNames!(markers)
+
+    names!(flowDF, Symbol.(markers), makeunique=true)
+    flowFrame[name] = flowDF
+
+    return flowFrame
+end
