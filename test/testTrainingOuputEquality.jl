8d57861c915c04f73179f5466640936b2c4d8cfd
laurentheirendt <laurent.heirendt@uni.lu>
2020-01-11 16:24:58 +0100

    integrate training test to overall test suite

--- test/testTrainingOuputEquality.jl
+++ test/testTrainingOuputEquality.jl
@@ -1,11 +1,13 @@
 Random.seed!(1)
 
-dataPath = "/Users/ohunewald/work/artificial_data_cytof"
 cd(dataPath)
-md = DataFrame(XLSX.readtable("metadata.xlsx", "Sheet1", infer_eltypes=true)...)
+
+md = DataFrame(XLSX.readtable("PBMC8_metadata.xlsx", "Sheet1", infer_eltypes=true)...)
 # take only the first 2 files for testing
 md = md[1:2, :]
-panel = DataFrame(XLSX.readtable("panel.xlsx", "Sheet1", infer_eltypes=true)...)
+panel = DataFrame(XLSX.readtable("PBMC8_panel.xlsx", "Sheet1", infer_eltypes=true)...)
+
+cd(genDataPath)
 
 lineageMarkers, functionalMarkers = getMarkers(panel)
 
@@ -17,7 +19,7 @@ generateIO(dataPath, md, nWorkers, true, 1, true)
 
 R =  Vector{Any}(undef,nWorkers)
 
-@time @sync for (idx, pid) in enumerate(workers())
+@sync for (idx, pid) in enumerate(workers())
     @async R[idx] = fetch(@spawnat pid loadData(idx, "input-$idx.jls", md, panel))
 end
 
@@ -27,16 +29,18 @@ som2Codes = deepcopy(som.codes)
 
 cc = map(Symbol, vcat(lineageMarkers, functionalMarkers))
 
-@time som = trainGigaSOM(som, R, cc)
+som = trainGigaSOM(som, R, cc)
 winners = mapToGigaSOM(som, R)
 embed = embedGigaSOM(som, R, k=10, smooth=0.0, adjust=0.5)
 
 # Load the data again using the "classic serial approach"
+cd(dataPath)
 fileNames = sort(md.file_name)
 fcsRaw = readFlowset(fileNames)
 cleanNames!(fcsRaw)
 
 # create daFrame file
+cd(genDataPath)
 daf = createDaFrame(fcsRaw, md, panel)
 dfSom = daf.fcstable[:,cc]
 
@@ -44,7 +48,7 @@ dfSom = daf.fcstable[:,cc]
 # to get the same starting som grid for training
 som2 = initGigaSOM(dfSom, 10, 10)
 som2.codes = som2Codes
-@time som2 = trainGigaSOM(som2, dfSom)
+som2 = trainGigaSOM(som2, dfSom)
 winners2 = mapToGigaSOM(som2, dfSom)
 embed2 = embedGigaSOM(som2, dfSom, k=10, smooth=0.0, adjust=0.5)
 
@@ -59,4 +63,3 @@ end
 end
 
 rmprocs(workers())
-cd(ENV["PWD"])
