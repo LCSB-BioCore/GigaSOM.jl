a0712d25078a3cd2f5e97bc570514c747568889a
laurentheirendt <laurent.heirendt@uni.lu>
2020-03-10 15:18:51 +0100

    remove devbranch

--- docs/make.jl
+++ docs/make.jl
@@ -26,6 +26,5 @@ deploydocs(
     repo = "github.com/LCSB-BioCore/GigaSOM.jl.git",
     target = "build",
     branch = "gh-pages",
-    devbranch = "origin/develop",
     versions = "stable" => "v^",
     )
