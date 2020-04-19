f62200e6e8647feafa539a78733389def4335cf9
laurentheirendt <laurent.heirendt@uni.lu>
2020-04-19 18:05:34 +0200

    cleanup of doc

--- v0.0.4/search_index.js
+++
@@ -1,3 +0,0 @@
-var documenterSearchIndex = {"docs":
-[{"location":"#","page":"GigaSOM.jl","title":"GigaSOM.jl","text":"(Image: GigaSOM.jl)","category":"page"},{"location":"#GigaSOM.jl-1","page":"GigaSOM.jl","title":"GigaSOM.jl","text":"","category":"section"},{"location":"#","page":"GigaSOM.jl","title":"GigaSOM.jl","text":"Huge-scale, high-performance flow cytometry clustering","category":"page"}]
-}
